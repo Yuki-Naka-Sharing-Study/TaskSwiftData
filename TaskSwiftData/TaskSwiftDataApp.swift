820c75f7a80da1686feeabc38a06ed8aa04905fb
仲優樹 <yu-ki216m@outlook.jp>
2025-07-01 08:05:36 +0900

    Initial Commit

---
+++ TaskSwiftData/TaskSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskSwiftDataApp.swift
+//  TaskSwiftData
+//
+//  Created by 仲優樹 on 2025/07/01.
+//
+
+import SwiftUI
+
+@main
+struct TaskSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
